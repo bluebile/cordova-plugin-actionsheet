2d2c759318cc50a839672f615cb06e15cb3b37fd
viniciusgandrade <viniciusgandrade@gmail.com>
2015-12-31 01:04:10 -0200

    Update ActionSheet.cs

--- src/wp8/ActionSheet.cs
+++ src/wp8/ActionSheet.cs
@@ -66,7 +66,7 @@ namespace Cordova.Extension.Commands
                 CordovaView cView = getCordovaView();
                 cView.Browser.Dispatcher.BeginInvoke(() =>
                 {
-                    cView.Browser.InvokeScript("execScript", "document.addEventListener('backbutton', window.plugins.actionsheet.hide, false)");
+                    //cView.Browser.InvokeScript("execScript", "document.removeEventListener('backbutton', window.plugins.actionsheet.hide, false)");
                     cView.Browser.Opacity = 0.5d;
                 });
 
